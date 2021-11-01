977758c6baf8cd3159f3687f52948982153aa645
vip933 <71464354+vip933@users.noreply.github.com>
2021-11-01 20:10:48 +0300

    Delete hw5_1 directory

--- hw5_1/src/Zappy/java/bse208/hw5/Main.java
+++
@@ -1,9 +0,0 @@
-package Zappy.java.bse208.hw5;
-
-public class Main {
-
-    public static void main(String[] args) {
-        String path = "test.txt";
-        new MyFileIterator(path).forEach(System.out::println);
-    }
-}
